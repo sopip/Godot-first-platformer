1404d91832fef04a308773f7a27d8cf24808b55f
sopop <162905162+sopip@users.noreply.github.com>
2024-09-17 10:34:58 +0200

    flagstang jubi

--- Scenes/next_level.gd
+++ Scenes/next_level.gd
@@ -4,7 +4,6 @@ func _on_body_entered(body):
 	if body.is_in_group("player"):  # hvis kollisionen er med spilleren
 		$CollisionShape2D.disabled = true
 		
-		
 		$AnimatedSprite2D.connect("animation_finished", Callable(self, "_on_animation_finished"))
 
 # når collected animationen er færdig: 
