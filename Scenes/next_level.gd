a10ebeb89a943371c825c98f8592b4dc655c3a2e
bentegetmoney <142792540+bentegetmoney@users.noreply.github.com>
2024-09-17 10:17:15 +0200

    next level

--- Scenes/next_level.gd
+++ Scenes/next_level.gd
@@ -1,11 +1,12 @@
 extends Area2D
 
+func _on_body_entered(body):
+	if body.is_in_group("player"):  # hvis kollisionen er med spilleren
+		$CollisionShape2D.disabled = true
+		
+		
+		$AnimatedSprite2D.connect("animation_finished", Callable(self, "_on_animation_finished"))
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+# når collected animationen er færdig: 
+func _on_animation_finished():
+	queue_free()  # collected animationen fjernes
