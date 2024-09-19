17ec0f33f615cf345f72640810a24a6a1a9f59d6
bentegetmoney <142792540+bentegetmoney@users.noreply.github.com>
2024-09-19 15:02:39 +0200

    enemy ny metode

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -1,51 +1,56 @@
 extends Node2D
 
 @onready var animated_sprite= $AnimatedSprite2D
-
+@onready var weapon_area = $WeaponArea
+@onready var collision_shape = $WeaponArea/CollisionShape2D
 
 var time_passed = 0  # Variable to store the accumulated time
 var enemy_hit = false
 var current_chameleon_animation = 1
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _physics_process(delta):
 	time_passed += delta  # Increment the time by the delta value
-
+	
+	if animated_sprite.is_playing():
+		if animated_sprite.frame in range(1,6):
+			collision_shape.shape = RectangleShape2D.new()
+			collision_shape.shape.extents = Vector2(0,0) # størrelse
+			collision_shape.position = Vector2(0, 0) # position
+		elif animated_sprite.frame in range(6,8): # lang tunge
+			collision_shape.shape = RectangleShape2D.new()
+			collision_shape.shape.extents = Vector2(36, 7)
+			collision_shape.position = Vector2(10, 0)
+		elif animated_sprite.frame == 8: # mellemlang tunge
+			collision_shape.shape = RectangleShape2D.new()
+			collision_shape.shape.extents = Vector2(25,7)
+			collision_shape.position = Vector2(10, 0)
+		elif animated_sprite.frame == 9: # kort tunge
+			collision_shape.shape = RectangleShape2D.new()
+			collision_shape.shape.extents = Vector2(10,7)
+			collision_shape.position = Vector2(10, 0)
+		else: 
+			collision_shape.shape = RectangleShape2D.new()
+			collision_shape.shape.extents = Vector2(0,0)
+			collision_shape.position = Vector2(0, 0)
+	
 	if time_passed >= 5:  # Check if 7 seconds have passed
 		print("time passed")
-		enemy_hit = true
+		#enemy_hit = true
 		time_passed = 0  # Reset the timer after performing the action
 		play_hit()
-		#$AnimatedSprite2D.connect("animation_finished", Callable(self, "hit2"))
+		$AnimatedSprite2D.connect("animation_finished", Callable(self, "play_idle"))
+
+func play_idle():
+	animated_sprite.play("chameleon_idle")
 
 func play_hit():
 	print("playhit")
-	current_chameleon_animation = 1
-	var chameleon_animation = "chameleon" + str(current_chameleon_animation)
-	animated_sprite.play(chameleon_animation)
-	if not animated_sprite.is_connected("animation_finished", Callable(self, "_on_hit_animation_finished")):
-		animated_sprite.connect("animation_finished", Callable(self, "_on_hit_animation_finished"))
+	animated_sprite.play("chameleon_attack")
+	set_physics_process(true)
 
 	#animated_sprite.connect("animation_finished", Callable(self, "_on_hit_animation_finished"))
 
-func _on_hit_animation_finished():
-	print("animationfin")
-	current_chameleon_animation +=1
-	
-	if current_chameleon_animation <= 3: # hvis der er flere animationer tilbage
-		print(current_chameleon_animation)
-		var chameleon_animation = "chameleon" + str(current_chameleon_animation)
-		animated_sprite.play(chameleon_animation)
-	else: 
-		#animated_sprite.disconnect("animation_finished", Callable(self, "_on_hit_animation_finished"))
-		enemy_hit = false
-		 
+func _on_weapon_hit(area):
+	Global.health -= 25
+	print("ouch! Dit health er nu: " , Global.health, "/100")
+	# her skal den trække fra health
