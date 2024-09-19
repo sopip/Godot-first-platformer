97ab1ea84de06176cf8905778ff3f7e7d37a5c66
bentegetmoney <142792540+bentegetmoney@users.noreply.github.com>
2024-09-19 13:33:46 +0200

    enemy

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
-@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var animated_sprite= $AnimatedSprite2D
+
+
+var time_passed = 0  # Variable to store the accumulated time
+var enemy_hit = false
+var current_chameleon_animation = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,6 +17,35 @@ func _process(delta):
 	pass
 
 
-# ændre animation en gang i mellem til hit
 func _physics_process(delta):
-	pass
+	time_passed += delta  # Increment the time by the delta value
+
+	if time_passed >= 5:  # Check if 7 seconds have passed
+		print("time passed")
+		enemy_hit = true
+		time_passed = 0  # Reset the timer after performing the action
+		play_hit()
+		#$AnimatedSprite2D.connect("animation_finished", Callable(self, "hit2"))
+
+func play_hit():
+	print("playhit")
+	current_chameleon_animation = 1
+	var chameleon_animation = "chameleon" + str(current_chameleon_animation)
+	animated_sprite.play(chameleon_animation)
+	if not animated_sprite.is_connected("animation_finished", Callable(self, "_on_hit_animation_finished")):
+		animated_sprite.connect("animation_finished", Callable(self, "_on_hit_animation_finished"))
+
+	#animated_sprite.connect("animation_finished", Callable(self, "_on_hit_animation_finished"))
+
+func _on_hit_animation_finished():
+	print("animationfin")
+	current_chameleon_animation +=1
+	
+	if current_chameleon_animation <= 3: # hvis der er flere animationer tilbage
+		print(current_chameleon_animation)
+		var chameleon_animation = "chameleon" + str(current_chameleon_animation)
+		animated_sprite.play(chameleon_animation)
+	else: 
+		#animated_sprite.disconnect("animation_finished", Callable(self, "_on_hit_animation_finished"))
+		enemy_hit = false
+		 
